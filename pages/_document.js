a252891ee24355dcf6d89e4a8757d6cbd9e1d70b
harsh.sabhaya <harsh.sabhaya@differenzsystem.com>
2025-03-28 13:00:38 +0530

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
